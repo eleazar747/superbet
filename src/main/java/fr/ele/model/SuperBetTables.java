255a3ce74ac294d21c9003e613c4bb17054b8b15
garc33 <garc33@gmail.com>
2013-07-17 10:19:16 +0200

    factorize code

--- src/main/java/fr/ele/model/SuperBetTables.java
+++ src/main/java/fr/ele/model/SuperBetTables.java
@@ -3,16 +3,15 @@ package fr.ele.model;
 public class SuperBetTables {
     public static final String ID_COLUMN = "ID";
 
+    public static final String CODE_COLUMN = "CODE";
+
     public static final class BookMakerTable {
         public static final String TABLE = "BOOKMAKER";
 
-        public static final String CODE_COLUMN = "CODE";
     }
 
     public static final class MatchTable {
         public static final String TABLE = "MATCH";
-
-        public static final String CODE_COLUMN = "CODE";
 
         public static final String DATE_COLUMN = "DATE";
 
@@ -23,14 +22,10 @@ public class SuperBetTables {
     public static final class SportTable {
         public static final String TABLE = "SPORT";
 
-        public static final String CODE_COLUMN = "CODE";
-
     }
 
     public static final class BetTypeTable {
         public static final String TABLE = "BET_TYPE";
-
-        public static final String CODE_COLUMN = "CODE";
 
     }
 
--- src/main/java/fr/ele/model/ref/BetType.java
+++ src/main/java/fr/ele/model/ref/BetType.java
@@ -1,28 +1,16 @@
 package fr.ele.model.ref;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.Proxy;
 
-import fr.ele.model.SuperBetEntity;
+import fr.ele.model.HasCodeEntity;
 import fr.ele.model.SuperBetTables;
 
 @Entity
 @Table(name = SuperBetTables.BetTypeTable.TABLE)
 @Proxy(proxyClass = BetType.class)
-public class BetType extends SuperBetEntity {
-
-    @Column(name = SuperBetTables.BetTypeTable.CODE_COLUMN, nullable = false, unique = true)
-    private String code;
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
+public class BetType extends HasCodeEntity {
 
 }
