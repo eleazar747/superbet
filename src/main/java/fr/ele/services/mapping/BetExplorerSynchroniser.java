56810bd0c9808550afacbe95bdfb04fcc6f8afd7
eleazar <eleazar.lemardeley@gmail.com>
2014-01-30 12:42:37 +0100

    early finished

--- src/main/java/fr/ele/services/mapping/BetExplorerSynchroniser.java
+++ src/main/java/fr/ele/services/mapping/BetExplorerSynchroniser.java
@@ -19,12 +19,18 @@ import fr.ele.model.Bet;
 import fr.ele.model.ref.BetType;
 import fr.ele.model.ref.Match;
 import fr.ele.model.ref.RefKey;
+import fr.ele.model.ref.Sport;
 
 @Service("BetExplorerSynchroniser")
 public class BetExplorerSynchroniser extends AbstractSynchronizer<Odds> {
 
 	private final String matchId = "matchid=";
 	private int matchCount = 0;
+	private final String FOOTBALL = "Football";
+	private final String BASEBALL = "Basketball";
+	private final String HANDBALL = "Handball";
+	private final String VOLLEYBALL = "Volleyball";
+	private final String HOCKEY = "Hockey";
 
 	@Override
 	protected long convert(SynchronizerContext context, Odds dto) {
@@ -32,9 +38,13 @@ public class BetExplorerSynchroniser extends AbstractSynchronizer<Odds> {
 		long nb = 0L;
 
 		try {
-			parseNextMatch("http://www.betexplorer.com/next/soccer/", context); // For
+			// parseNextMatch("http://www.betexplorer.com/next/soccer/",
+			// FOOTBALL,
+			// context); // For
 			// basketball
-			// parseNextMatch("http://www.betexplorer.com/next/volleyball/"); //
+			parseNextMatch("http://www.betexplorer.com/next/volleyball/",
+					VOLLEYBALL, context);
+			// //
 			// for
 			// Volleyball
 		} catch (Throwable e) {
@@ -45,16 +55,17 @@ public class BetExplorerSynchroniser extends AbstractSynchronizer<Odds> {
 		return nb;
 	}
 
-	private void parseNextMatch(String httpRef, SynchronizerContext context)
-			throws Throwable {
+	private void parseNextMatch(String httpRef, String sportType,
+			SynchronizerContext context) throws Throwable {
 		// TODO Auto-generated method stub
 		URL website = new URL(httpRef);
-		URLConnection urlConnetion = website
-				.openConnection(getProxy(httpRef));
-		//Document doc = Jsoup
-			//	.parse(urlConnetion.getInputStream(), null, httpRef);
-		Document doc = Jsoup.connect(httpRef).get();
+		URLConnection urlConnetion = website.openConnection(getProxy(httpRef));
+		Document doc = Jsoup
+				.parse(urlConnetion.getInputStream(), null, httpRef);
+		// Document doc = Jsoup.connect(httpRef).get();
 		org.jsoup.select.Elements e = doc.select("tr");
+		// Define sport
+		Sport sport = context.findSport(sportType);
 
 		// Search URL for each match
 		for (Element t : e) {
@@ -77,16 +88,19 @@ public class BetExplorerSynchroniser extends AbstractSynchronizer<Odds> {
 							String extract = linkHref.substring(
 									linkHref.length() - 9,
 									linkHref.length() - 1);
-	
+							// before, need to retrive match team1 vs team 2;
 							String linkOdd1 = "http://www.betexplorer.com/gres/ajax-matchodds.php?t=n&e="
 									+ extract + "&b=ou";
-							parseMatchId(linkOdd1, context);
-							/*String linkOdd2 = "http://www.betexplorer.com/gres/ajax-matchodds.php?t=n&e="
-									+ extract + "&b=1x2";
-							parseMatchId(linkOdd2, context);
-							String linkOdd3 = "http://www.betexplorer.com/gres/ajax-matchodds.php?t=n&e="
-									+ extract + "&b=ou";
-							parseMatchId(linkOdd3, context);*/
+							parseMatchId(linkOdd1, sport, context);
+							/*
+							 * String linkOdd2 =
+							 * "http://www.betexplorer.com/gres/ajax-matchodds.php?t=n&e="
+							 * + extract + "&b=1x2"; parseMatchId(linkOdd2,
+							 * context); String linkOdd3 =
+							 * "http://www.betexplorer.com/gres/ajax-matchodds.php?t=n&e="
+							 * + extract + "&b=ou"; parseMatchId(linkOdd3,
+							 * context);
+							 */
 							System.out.println(matchCount + "," + linkOdd1);
 						}
 
@@ -96,22 +110,22 @@ public class BetExplorerSynchroniser extends AbstractSynchronizer<Odds> {
 		}
 	}
 
-	private long parseMatchId(String httpRef, SynchronizerContext context)
-			throws Throwable {
+	private long parseMatchId(String httpRef, Sport sport,
+			SynchronizerContext context) throws Throwable {
 		long nb = 0L;
 		URL website = new URL(httpRef);
-		URLConnection urlConnetion = website
-				.openConnection(getProxy(httpRef));
-		//Document docmatch = Jsoup.parse(urlConnetion.getInputStream(), null,
-			//	httpRef);
-		Document docmatch = Jsoup.connect(httpRef).get();
+		URLConnection urlConnetion = website.openConnection(getProxy(httpRef));
+		Document docmatch = Jsoup.parse(urlConnetion.getInputStream(), null,
+				httpRef);
+		// Document docmatch = Jsoup.connect(httpRef).get();
 		if (docmatch.select("tr").isEmpty() == false) {
 			org.jsoup.select.Elements e = docmatch.select("tr");
+
 			// One bet Type, one Bookmaker, one match
 
 			for (Element t : e) {
-				org.jsoup.select.Elements f=t.select("td");
-				parseOverUnder(f);
+				org.jsoup.select.Elements f = t.select("td");
+				parseOverUnder(f, t);
 
 			}
 		}
@@ -119,16 +133,51 @@ public class BetExplorerSynchroniser extends AbstractSynchronizer<Odds> {
 		return nb;
 	}
 
-	private void parseOverUnder(org.jsoup.select.Elements elements){
-		
-		for(Element r : elements){
-			String str=r.text();
-			String tmp=r.attr("td.data-odd");
-			System.out.println(str);
+	private void parseOverUnder(org.jsoup.select.Elements elements, Element t) {
+		// Over Under structure <tr> 4 elements : 1/nothing : 2/ref over under
+		// 3/ odd Over 4/odd under
+		Element tnode = t.select("th").first();
+		String hstr = tnode.attr("span.class");
+		int count = 0;
+
+		for (Element r : elements) {
+			count++;
+			if (count == 2) {
+
+				String str = r.text().replaceAll("/", "").replaceAll("td", "")
+						.replaceAll("sets", "").replaceAll("<", "")
+						.replaceAll(">", "").replace(" \\", "");
+				str = "Over/Under " + str;
+				System.out.println(str);
+
+			}
+			if (count == 3) {
+
+				String odd1 = r.attr("data-odd").replaceAll("/", "")
+						.replaceAll("<", "").replaceAll(">", "")
+						.replace("\\", "").replace("\"", "");
+				System.out.println(odd1);
+			}
+			if (count == 4) {
+
+				String odd2 = r.attr("data-odd").replaceAll("/", "")
+						.replaceAll("<", "").replaceAll(">", "")
+						.replace("\\", "").replace("\"", "");
+
+				System.out.println(odd2);
+
+			}
+
 		}
-		
-		
+		/**
+		 * Match match = context.findOrCreateMatch(sport, matchBcDto
+		 * .getStartDate().toGregorianCalendar().getTime(), player1, player2);
+		 * 
+		 * BetType betType = context.findBetType(betName);
+		 */
+
 	}
+
 	@Override
 	protected Class<Odds> getDtoClass() {
 		// TODO Auto-generated method stub
@@ -143,7 +192,6 @@ public class BetExplorerSynchroniser extends AbstractSynchronizer<Odds> {
 		return proxy;
 
 	}
-
 
 	// Optional : maybe to create a historical.
 	private void convert(Date date, String odd, Match match, String betTypes,
