28ace38e1df30f7455556cfc0763e44a5f4222f8
garc33 <marcherman@192.168.1.113>
2014-05-08 14:31:19 +0200

    add statistics

--- src/main/java/fr/ele/services/rest/impl/BetTypeRestServiceImpl.java
+++ src/main/java/fr/ele/services/rest/impl/BetTypeRestServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.codahale.metrics.annotation.Timed;
 import com.codiform.moo.curry.Translate;
 import com.google.common.collect.Lists;
 
@@ -23,14 +24,15 @@ import fr.ele.services.rest.BetTypeRestService;
 @Transactional(readOnly = true)
 @Service(BetTypeRestService.SERVER)
 public class BetTypeRestServiceImpl extends
-        AbstractRefRestServiceImpl<BetTypeDto, BetType> implements
-        BetTypeRestService {
+AbstractRefRestServiceImpl<BetTypeDto, BetType> implements
+BetTypeRestService {
 
     @Autowired
     private BetTypeRepository betTypeRepository;
 
     @Override
     @Transactional
+    @Timed
     public BetTypeDto create(BetTypeDto betType) {
         return super.create(betType);
     }
@@ -46,23 +48,27 @@ public class BetTypeRestServiceImpl extends
     }
 
     @Override
+    @Timed
     public Iterable<BetTypeDto> findAll() {
         return super.findAll();
     }
 
     @Override
     @Transactional
+    @Timed
     public void delete(long id) {
         super.delete(id);
     }
 
     @Override
     @Transactional
+    @Timed
     public List<BetTypeDto> insertCsv(Attachment file) {
         return insertCsv(file, BetType.class);
     }
 
     @Override
+    @Timed
     public Iterable<BetTypeDto> search(BetTypeSearch betTypeSearch) {
         QueryBuilder queryBuilder = new QueryBuilder();
         SearchMapping.map(queryBuilder, entityPath(), betTypeSearch);
@@ -81,11 +87,13 @@ public class BetTypeRestServiceImpl extends
     }
 
     @Override
+    @Timed
     public BetTypeDto findByCode(String code) {
         return super.findByCode(code);
     }
 
     @Override
+    @Timed
     public BetTypeDto get(long id) {
         return super.get(id);
     }
